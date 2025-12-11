8c08341b65b5966b5d3af7a9419f62c3d9724c31
I. F. F. dos Santos <53145011+ismaeldamiao@users.noreply.github.com>
2025-12-11 13:42:19 -0300

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'background': "#0E1111",
+                'primary': "#025930",
+                'secondary': "#8EDBAF",
+                'text-primary': "#DFD9C3",
+                'text-secondary': "#9DA2A6",
+            }
+        }
+    }
+}
